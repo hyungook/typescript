6be3a76bd300556a3a61c432885c6ac63cbeb20a
오형욱 <nenonenoberneno@gmail.com>
2021-04-01 20:41:07 +0900

    types-union

--- 1-types/1-4-alias.ts
+++ 1-types/1-4-alias.ts
@@ -29,4 +29,5 @@
   const json: JSON = 'json';
 
   type Boal = true;
+  
 }
--- 1-types/1-5-union.ts
+++ 1-types/1-5-union.ts
@@ -1,16 +1,18 @@
 {
   /**
-   *  Union Types: OR
+   *  Union Types: OR ⭐️
    */
   type Direction = 'left' | 'right' | 'up' | 'down';
   function move(direction: Direction) {
     console.log(direction);
   }
-  move('down');
+  move('down'); // 'left' | 'right' | 'up' | 'down' 가능한 인자 1가지만 선택할 수 있다.
 
   type TileSize = 8 | 16 | 32;
   const tile: TileSize = 16;
 
+
+  // 실전 예제
   // function: login -> success, fail ⏱
   type SuccessState = {
     response: {
@@ -20,8 +22,10 @@
   type FailState = {
     reason: string;
   };
+
   type LoginState = SuccessState | FailState;
 
+  // function login(id: string, password: string): Promise<LoginState> {
   function login(): LoginState {
     return {
       response: {
@@ -33,11 +37,18 @@
   // printLoginState(state: LoginState)
   // success -> 🎉 body
   // fail -> 😭 reason
-  function printLoginState(state: LoginState) {
+  function printLoginState(state: LoginState):void {
     if ('response' in state) {
       console.log(`🎉 ${state.response.body}`);
     } else {
       console.log(`😭 ${state.reason}`);
     }
   }
+
+
+
+
+
+
+
 }
